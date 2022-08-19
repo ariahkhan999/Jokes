d660b3715b66e70eae2e66d43afdd47976a3e9cc
ariahkhan999 <se120182010@gmail.com>
2022-08-19 11:30:55 -0700

    added files

---
+++ script.js
@@ -0,0 +1,44 @@
+{
+  /* <div id="joke" class="joke">Awesome Joke is loading.....</div>
+      <button id="jokeBtn" class="Btn">Next Joke</button> */
+}
+
+// Promise
+// GET https://icanhazdadjoke.com/
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+// const generateJokes = () => {
+//   const setHeader = {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   };
+//   fetch("https://icanhazdadjoke.com/", setHeader)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokes.innerHTML = data.joke;
+//     })
+
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+const generateJokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await fetch("https://icanhazdadjoke.com/", setHeader);
+    const data = await res.json();
+    jokes.innerHTML = data.joke;
+  } catch (err) {
+    console.log(`the error is ${err}`);
+  }
+};
+
+jokeBtn.addEventListener("click", generateJokes);
+generateJokes();
